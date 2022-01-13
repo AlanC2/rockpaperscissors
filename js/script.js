6badf8c731d48cf523fbfa200c2e1255f9cfcf9a
Glitch (as-rock-paper-scissors) <none>
2022-01-13 01:52:04 +0000

    🍥🖊️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
+let randomNumber = 0;
+$(".play").click(function(){
+  let userInput = $(".input").val();
+  $(".userChoice").text(userInput);
+  
+  
+  randomNumber = Math.random();
+   randomNumber = Math.random()*3;
+   randomNumber = Math.ceil(randomNumber);
+  $(".computerChoice").text(randomNumber)
+  if (randomNumber  === 1) {
+    $(".computerChoice").text ("Rock"); }
+    if (randomNumber  === 2) {
+    $(".computerChoice").text ("Paper"); }
+    if (randomNumber  === 3) {
+    $(".computerChoice").text ("Scissors"); }
+  
 
+})
+let result = "";
+function compare(UserChoice, computerChoice) {
+    if (UserChoice === 1 && computerChoice === 1) {
+        return "Tie";
+    }
+    else if (UserChoice === 1 && computerChoice === 2) {
+        return "Computer wins";
+    }
+    else if (UserChoice === 1 && computerChoice === 3) {
+        return "User wins";
+    }
+  else if (UserChoice === 2 && computerChoice === 1) {
+        return "User wins";
+  }
+  else if (UserChoice === 2 && computerChoice === 2) {
+        return "Tie"
+  }
+  else if (UserChoice === 2 && computerChoice === 3) {
+        return "Computer wins";
+  }
+  else if (UserChoice === 3 && computerChoice === 1) {
+        return "User wins";
+}
+  else if (UserChoice === 3 && computerChoice === 2) {
+        return "User wins";
+  }
+  else if (UserChoice === 3 && computerChoice === 3) {
+        return "User wins";
+}
+  
+  $
+  $(".input").val("");
+  
+  
+  
+  
+}
